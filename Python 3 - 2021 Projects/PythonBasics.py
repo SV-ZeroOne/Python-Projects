0bcef1737fae4b17bcbac5f564c4490f7a86d987
Steve Velcev <steve@elitehost.co.za>
2021-09-21 19:40:04 +0100

    Add basicpython.py script

---
+++ Python 3 - 2021 Projects/PythonBasics.py
@@ -0,0 +1,24 @@
+# 21 Sep 2021 - Python for Beginners Udemy Refresher 
+print("Hello Steve, its been a while!")
+print("Lets do some simple arithmetic math.")
+print(400 + 20)
+print("To the power of **")
+print(10 ** 3)
+print('-' * 10)
+print('Variables')
+print('-' * 10)
+example_var = "Variable names are case sensitive and cannot start with a number. They can only contain alpha-numeric characters (A-z, 0-9 and _)"
+print(example_var)
+print('snake_case is preferred over camelCase')
+
+"""
+Multi
+Line
+Comment
+"""
+
+'''
+Can use single or double quotes 
+for multi-line commnets
+'''
+
